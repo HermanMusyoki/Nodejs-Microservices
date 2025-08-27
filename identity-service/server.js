722b5326d59d43828af676def786d263effb3f54
HermanMusyoki <salmantso14@gmail.com>
2025-08-27 09:03:22 +0300

    refresh token for users

--- identity-service/server.js
+++ identity-service/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const logger = require("./utils/logger");
+const logger = require("./src/utils/logger");
 const express = require("express");
 
 const app = express();
